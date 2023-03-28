ac3c5f0076b1db7503117a114d672ea73a7267b8
bl0pez <blopez4434@gmail.com>
2023-03-28 16:40:51 -0300

    agregando mas repos

--- src/controllers/seed.js
+++ src/controllers/seed.js
@@ -4,7 +4,12 @@ const seed = async(req, res) => {
 
         try {
         //Solicitud a la api de github
-        const response = await fetch('https://api.github.com/users/bl0pez/subscriptions');
+        const response = await fetch('https://api.github.com/users/bl0pez/repos', {
+            headers: {
+                'Authorization': `token ${process.env.GITHUB_TOKEN}`
+            }
+        }); //https://api.github.com/users/bl0pez/subscriptions
+        //https://api.github.com/users/bl0pez/repos
         const githubData = await response.json();
 
         //Limpiamos la base de datos
@@ -20,28 +25,31 @@ const seed = async(req, res) => {
             topics,
             homepage,
         }) => {
+
+                console.log(name);
                 
                 const imageURL = `https://raw.githubusercontent.com/bl0pez/${name}/master/preview.png`;
 
                 //Agregamos solo los que tengan la etiqueta frontend y backend
-                if(!topics.includes('frontend') && !topics.includes('backend')) return;
+                if(topics.includes('frontend') || topics.includes('backend') || !!html_url) {
+                    repos.push({
+                        name,
+                        description,
+                        html_url,
+                        image: imageURL,
+                        topics,
+                        homepage,
+                    });
+                }
 
-
-                repos.push({
-                    name,
-                    description,
-                    html_url,
-                    image: imageURL,
-                    topics,
-                    homepage,
-                });
         });
 
         await Github.insertMany(repos);
 
         res.status(200).json({
             ok: true,
-            msg: "Data saved"
+            msg: "Data saved",
+            total: repos.length
         });
 
     } catch (error) {
